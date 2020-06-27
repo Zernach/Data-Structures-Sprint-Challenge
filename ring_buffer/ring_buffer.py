1b520c21933f47be8670318d9418b78df9981a24
Zernach <ryan@zernach.com>
2020-06-26 21:31:44 -0400

    finished RingBuffer module yay - just needed to step away and a take a breather from it

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,18 +1,19 @@
 class RingBuffer:
-    def __init__(self, capacity, data=[]):
+    def __init__(self, capacity):
         self.capacity = capacity
-        self.data = data
-        self.size = len(self.data)
+        self.data = []
+        self.size = 0
         self.oldest_index = 0
 
     def append(self, item):
         if self.size < self.capacity:
             # in this instance, the RingBuffer is NOT yet at max capacity
             self.data.append(item)
+            self.size += 1
         else:
             # in this instance, the RingBuffer is at max capacity
             #remove oldest item from data
-            oldest_item = self.data.pop(self.oldest_index)
+            self.data.pop(self.oldest_index)
             # insert item into data at oldest_item index
             self.data.insert(self.oldest_index, item)
             # change oldest index
